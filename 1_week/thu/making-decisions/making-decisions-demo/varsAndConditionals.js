e50ec7a22f9e14d629987df247db3c226ec88c32
Daniel Zeh <dfzeh1@gmail.com>
2023-06-22 11:41:52 -0500

    "made program with only attack"

---
+++ 1_week/thu/making-decisions/making-decisions-demo/varsAndConditionals.js
@@ -0,0 +1,16 @@
+/*
+    In this file, we'll be using variables, conditionals,
+    and loops to play out a fight between Jon Snow and
+    Jamie Lannister.
+*/ 
+
+let jonSnowAttack = 25
+let jaimeLannisterAttack = 25
+
+if(jonSnowAttack > jaimeLannisterAttack) {
+    console.log(`Jon's attack is greather than Jaime's`)
+} else if(jaimeLannisterAttack > jonSnowAttack) {
+    console.log(`Jaime's attack is greater`)
+} else {
+    console.log(`its a tie`)
+}
