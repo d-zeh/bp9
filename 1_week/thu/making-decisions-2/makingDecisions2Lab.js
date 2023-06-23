13fbde5038bdcd23672ed08209397b56b559f505
Daniel Zeh <dfzeh1@gmail.com>
2023-06-23 16:07:44 -0500

    lettergrade ===

--- 1_week/thu/making-decisions-2/makingDecisions2Lab.js
+++ 1_week/thu/making-decisions-2/makingDecisions2Lab.js
@@ -157,19 +157,19 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 switch(letterGrade) {
-  case A: 
+  case letterGrade === A: 
     console.log("The student is doing excellently.");
     break;
-  case B:
+  case letterGrade === B:
     console.log("The student is doing well.");
     break;
-  case C: 
+  case letterGrade === C: 
     console.log("The student is doing alright.");
     break;
-  case D: 
+  case letterGrade === D: 
     console.log("The student is not doing very well.");
     break;
-  case F: 
+  case letterGrade === F: 
     console.log("The student is failing.");
     break;
   default:
